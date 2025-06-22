1b85147ffc89d3c3ae4cc3cd0599067438ad5d1e
Jacques Lucke <mail@jlucke.com>
2025-06-22 23:25:59 +0200

    initial content collection

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+import { glob } from "astro/loaders";
+import { z, defineCollection } from "astro:content";
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.md", base: "./src/blog" }),
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collections = { blog };

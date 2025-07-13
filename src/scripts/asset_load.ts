027bd33a397268419f51e5dbf5d9195cade3b6dc
Jacques Lucke <mail@jlucke.com>
2025-07-13 15:26:19 +0200

    simplify asset loading

---
+++ src/scripts/asset_load.ts
@@ -0,0 +1,19 @@
+import type { ImageMetadata } from "astro";
+
+const allAssets = import.meta.glob("/src/images/**/*.{jpg,webp,png,jpeg,mp4}");
+
+console.log(allAssets);
+
+export async function loadImage(path: string): Promise<ImageMetadata> {
+  if (!allAssets[path]) {
+    throw new Error(`Could not find image at ${path}`);
+  }
+  return ((await allAssets[path]()) as { default: ImageMetadata }).default;
+}
+
+export async function loadVideo(path: string) {
+  if (!allAssets[path]) {
+    throw new Error(`Could not find video at ${path}`);
+  }
+  return ((await allAssets[path]()) as { default: string }).default;
+}

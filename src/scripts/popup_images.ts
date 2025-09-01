d01a544c687f86076454c6c396a21f6f6fef7cdf
Jacques Lucke <mail@jlucke.com>
2025-09-02 00:26:36 +0200

    support back navigation for closing images

--- src/scripts/popup_images.ts
+++ src/scripts/popup_images.ts
@@ -2,22 +2,36 @@ const dialogElem: HTMLElement = document.getElementById("image-popup-dialog")!;
 const dialogImageElem: HTMLImageElement = document.getElementById(
   "image-popup-dialog-image",
 )! as HTMLImageElement;
-const popupImageElems = document.getElementsByClassName("popup-image");
+const popupImageElems = Array.from(
+  document.getElementsByClassName("popup-image"),
+).filter((elem) => elem instanceof HTMLImageElement);
 
 async function setup() {
   for (const imageElem of popupImageElems) {
     imageElem.addEventListener("click", (event) => {
-      triggerPopup(event.target as HTMLImageElement);
+      openPopup(event.target as HTMLImageElement);
     });
   }
 
   dialogElem.addEventListener("click", () => {
     closePopup();
   });
+
+  window.addEventListener("popstate", (event) => {
+    if (isPopupOpen()) {
+      closePopup(true);
+    }
+  });
 }
 
-function triggerPopup(imageElem: HTMLImageElement) {
-  const alreadyLoadedSrc = imageElem.getAttribute("src")!;
+function isPopupOpen() {
+  return !dialogElem.classList.contains("hidden");
+}
+
+function openPopup(imageElem: HTMLImageElement) {
+  if (isPopupOpen()) {
+    return;
+  }
   const highresSrc = imageElem.getAttribute("data-popup-src")!;
   dialogImageElem.src = "";
   dialogImageElem.width = parseInt(imageElem.getAttribute("data-popup-width")!);
@@ -27,11 +41,24 @@ function triggerPopup(imageElem: HTMLImageElement) {
   dialogImageElem.src = highresSrc;
   dialogElem.classList.remove("hidden");
   document.body.style.overflow = "hidden";
+
+  history.pushState({ isPopupImage: true }, "", "#image");
 }
 
-function closePopup() {
+function closePopup(fromPop: boolean = false) {
+  if (!isPopupOpen()) {
+    return;
+  }
   dialogElem.classList.add("hidden");
   document.body.style.overflow = "";
+
+  if (!fromPop) {
+    if (history.state && history.state.isPopupImage) {
+      history.back();
+    } else {
+      history.pushState(null, "", location.pathname + location.search);
+    }
+  }
 }
 
 setup();

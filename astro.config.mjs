26c73a7377116f56a31d2da29e7807a981273735
Jacques Lucke <mail@jlucke.com>
2026-06-27 13:07:51 +0200

    update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
+import { unified } from '@astrojs/markdown-remark';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,17 +13,18 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
   markdown: {
-    rehypePlugins: [
-      [rehypeSlug, {}],
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: "wrap",
-        },
+    processor: unified({
+      rehypePlugins: [
+        [rehypeSlug, {}],
+        [
+          rehypeAutolinkHeadings,
+          {
+            behavior: "wrap",
+          },
+        ],
       ],
-    ],
+    }),
   },
 
   integrations: [mdx()],
